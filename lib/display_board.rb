209912f78eb2e8a090fa28ebadd63ee549074297
Eva Leake <teasacuraster@gmail.com>
2018-03-13 23:54:37 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,11 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board(board)
+  cell = "   "
+  row = cell + "|" + cell + "|" + cell
+  puts row
+  puts "-----------"
+  puts row
+  puts "-----------"
+  puts row
+end
