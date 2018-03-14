1b4afe4a6d6e95a9af455d73f12111756d0b4eb1
Eva Leake <teasacuraster@gmail.com>
2018-03-14 00:01:45 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,10 +2,12 @@
 # out the current state.
 def display_board(board)
   cell = "   "
-  row = cell + "|" + cell + "|" + cell
-  puts row
+  row1 = "#{board[0]}" + "|" + "#{board[1]}" + "|" + "#{board[2]}"
+  row2 = "#{board[3]}" + "|" + "#{board[4]}" + "|" + "#{board[5]}"
+  row3 = "#{board[6]}" + "|" + "#{board[7]}" + "|" + "#{board[8]}"
+  puts row1
   puts "-----------"
-  puts row
+  puts row2
   puts "-----------"
-  puts row
+  puts row3
 end
